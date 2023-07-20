d05c7de7a9b23c6f99405af40cc2157a55e5ee8b
robrop <robrop@users.noreply.github.com>
2023-07-20 03:50:57 +0200

    Add enums for items

---
+++ enums.lua
@@ -0,0 +1,75 @@
+Resources = {
+	-- Raw
+	COAL             = "coal",
+	STONE            = "stone",
+	-- Ore
+	IRON             = "iron",
+	COPPER           = "copper",
+	LEAD             = "lead",
+	TITANIUM         = "titanium",
+	ZINC             = "zinc",
+	NICKEL           = "nickel",
+	STEEL            = "steel",
+	BRASS            = "brass",
+	INVAR            = "invar",
+	GALVANIZED_STEEL = "galvanized-steel"
+}
+
+Stock = {
+	PLATE     = "plate",
+	SHEET     = "sheet",
+	SQUARE    = "square",
+	ANGLE     = "angle",
+	GIRDER    = "girder",
+	WIRE      = "wire",
+	GEAR      = "gear",
+	FINE_GEAR = "fine-gear",
+	PIPE      = "pipe",
+	FINE_PIPE = "fine-pipe"
+}
+
+Machined_Part = {
+	PANELING       = "paneling",
+	LARGE_PANELING = "large-paneling",
+	FRAMING        = "framing",
+	GIRDERING      = "girdering",
+	GEARING        = "gearing",
+	FINE_GEARING   = "fine-gearing",
+	PIPING         = "piping",
+	FINE_PIPING    = "fine-piping",
+	WIRING         = "wiring",
+	SHIELDING      = "shielding",
+	SHAFTING       = "shafting",
+	BOLTS          = "bolts",
+	RIVETS         = "rivets"
+}
+
+Machined_Part_Property = {
+	BASIC                   = "basic",
+	LOAD_BEARING            = "load-bearing",
+	ELECTRICALLY_CONDUCTIVE = "electrically-conductive",
+	HIGH_TENSILE            = "high-tensile",
+	CORROSION_RESISTANT     = "corrosion-resistant",
+	LIGHTWEIGHT             = "lightweight",
+	DUCTILE                 = "ductile",
+	THERMALLY_STABLE        = "thermally-stable",
+	THERMALLY_CONDUCTIVE    = "thermally-conductive",
+	RADIATION_RESISTANT     = "radiation-resistant",
+	VERY_HIGH_TENSILE       = "very-high-tensile",
+	HEAVY_LOAD_BEARING      = "heavy-load-bearing",
+	HIGH_MELTING_POINT      = "high-melting-point"
+}
+
+Minisembler = {
+	WELDER         = "welder",
+	DRILL_PRESS    = "drill-press",
+	GRINDER        = "grinder",
+	METAL_BANDSAW  = "metal-bandsaw",
+	METAL_EXTRUDER = "metal-extruder",
+	MILL           = "mill",
+	METAL_LATHE    = "metal-lathe",
+	THREADER       = "threader",
+	SPOOLER        = "spooler",
+	ROLLER         = "roller",
+	BENDER         = "bender"
+}
